ced938470ca16bb6ded12c93e2342028c66692a7
Kalyan Shilpa Rani <kalyanshilpa2003@gmail.com>
2026-03-04 20:54:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+   origin: "frontend-q24la55j6-shilpas-projects-559ec8e3.vercel.app"
+}));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
-const loginLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, 
-  max: 5, 
-  message: {
-    message: "Too many login attempts. Try again later."
-  },
-});
+const PORT =  5000;
 
 
-app.post("/login", loginLimiter, (req, res) => {
+
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   if (username === "admin" && password === "admin") {
